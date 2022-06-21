7f1ec5c073a0932741fa4d599d78f8e4538a51ff
Mafiu <mf.milewski@gmail.com>
2022-06-21 20:55:42 -0300

    First map texture load added

--- src/ZeldaClone.cpp
+++ src/ZeldaClone.cpp
@@ -1,22 +1,28 @@
 #include "raylib.h"
 
-int windowsDimensions[2]{800,600};
-
+int windowsDimensions[2]{768, 704};
+const char* MAP_TEXTURE_PATH = "src/resources/maps/map_01.png";
 
 int main()
 {
+
     InitWindow(windowsDimensions[0], windowsDimensions[1], "Zelda Clone Game");
+
+    Texture2D map = LoadTexture(MAP_TEXTURE_PATH);
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(BLACK);
 
-  
+        float mapTextureScaleMultiplier = 1.5;
+        Vector2 mapPos{0.0, windowsDimensions[1] - (map.height * mapTextureScaleMultiplier)};
+        DrawTextureEx(map, mapPos, 0.0, mapTextureScaleMultiplier, WHITE);
 
         EndDrawing();
     }
 
+    UnloadTexture(map);
     CloseWindow();
 }
